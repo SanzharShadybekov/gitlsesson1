f3a92acbdb4e4ed78f79514a59d67dc282e4a312
marat <emarrgroup@gmail.com>
2022-04-26 21:06:20 +0600

    Marat

--- team.py
+++ team.py
@@ -1,11 +1,7 @@
 print('Hello my name is Sanzhar')
 print('Hello my name is Test')
 print('Hello my name is Beka')
-<<<<<<< HEAD
-
-=======
 print('Hello my name is Elzar')
 print('Hello my name is Chygyz')
 print("Hello my name is Bekbolsyn")
->>>>>>> 97a9c5a73e52b05c4fdfe537a582e75d85c9c479
 print('Hello my name is Marat')
